8317b502926be6d3edde82af51ded9067f1ba8fa
MubarakOke <okeolamubarak18@gmail.com>
2023-04-17 12:16:21 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-use std::net::TcpListener;
-
 fn main() {
-    let server= TcpListener::bind("127.0.0.1:7001").expect("didn't bind");
-    server.set_nonblocking(true).expect("failed in non_blocking");
-    let (val, valu)= server.accept().expect("unable to accept");
 
 }
