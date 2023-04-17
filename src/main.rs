afa5b4fecd5e38c00dea1f3db1156e8aa83a5993
MubarakOke <okeolamubarak18@gmail.com>
2023-04-17 13:42:17 +0100

    setup client connection

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ use std::io::{Write, Read, ErrorKind};
 use std::thread::{self, sleep};
 #[allow(unused_imports)]
 use std::sync::mpsc;
+use std::time::Duration;
 
-const LOCAL: String= String::from("127.0.0.1:7001");
+const LOCAL: &str= "127.0.0.1:7007";
 
+#[allow(unused_variables)]
 fn main() {
     println!("setting up server");
     let server= TcpListener::bind(LOCAL).expect("failed to setup server");
@@ -16,6 +18,42 @@ fn main() {
     println!("finished setting up server");
 
     let mut clients= vec![];
-    println!("finished setting up server");
+    let (tx, rx)= mpsc::channel::<String>();
 
+    loop {
+        if let Ok((mut client, addr)) = server.accept() {
+            println!("client connected at {}", addr);
+            let info= (client.try_clone().expect("unable to clone"), addr);
+            clients.push(info);
+            let tx= tx.clone();
+
+            thread::spawn(move || {
+                loop {
+                    let mut msg= String::new();
+                    match client.read_to_string(&mut msg){
+                        Ok(_)=>{
+                            println!("message {} form {:?}", msg, addr);
+                            tx.send(msg).expect("unable to send message");
+                        },
+                        Err(e) if e.kind() == ErrorKind::WouldBlock => {},
+                        Err(e) => {
+                            println!("error reading message {}", e);
+                            println!("closing connection with {}", addr);
+                            break;
+                        }
+                    }
+                    sleep_func();
+                };
+                }   
+            );
+            
+
+
+        }
+    }
+
+}
+
+fn sleep_func(){
+    sleep(Duration::from_millis(100));
 }
